3e228a1b0441869eed43dc8178b5825e376432c0
yumateb <yumateb@gmail.com>
2025-04-04 13:47:19 +0700

    init: initializ project

---
+++ src/main/java/com/practice/javagroupiiminiproject/JavaGroupIiMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practice.javagroupiiminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGroupIiMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGroupIiMiniProjectApplication.class, args);
+    }
+
+}
