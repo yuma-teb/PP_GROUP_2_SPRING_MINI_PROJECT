23130e3f41db1870e3ff9eacdc9033b1cc839f83
Noev Chanmakara <jrmakara97@gmail.com>
2025-04-06 13:26:08 +0700

    done getAllAchievement

---
+++ src/main/java/com/practice/javagroupiiminiproject/repository/AchievementRepository.java
@@ -0,0 +1,4 @@
+package com.practice.javagroupiiminiproject.repository;
+
+public interface AchievementRepository {
+}
---
+++ src/main/java/com/practice/javagroupiiminiproject/service/impl/AchievementServiceImpl.java
@@ -0,0 +1,4 @@
+package com.practice.javagroupiiminiproject.service.impl;
+
+public class AchievementServiceImpl {
+}
