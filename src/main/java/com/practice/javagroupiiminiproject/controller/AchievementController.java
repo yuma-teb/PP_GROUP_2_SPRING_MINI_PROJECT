9da3a79a80a393d973d926ba6d11c236679e76f3
Noev Chanmakara <jrmakara97@gmail.com>
2025-04-06 13:29:09 +0700

    done getAllAchievement

--- src/main/java/com/practice/javagroupiiminiproject/controller/AchievementController.java
+++ src/main/java/com/practice/javagroupiiminiproject/controller/AchievementController.java
@@ -1,4 +1,28 @@
 package com.practice.javagroupiiminiproject.controller;
 
-public class AcheivementController {
+import com.practice.javagroupiiminiproject.model.entity.Achievement;
+import com.practice.javagroupiiminiproject.model.response.APIResponse;
+import com.practice.javagroupiiminiproject.service.AchievementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/achievement")
+public class AchievementController {
+    private final AchievementService service;
+
+
+    @GetMapping
+    public ResponseEntity<APIResponse<List<Achievement>>> getAllAchievement(){
+        return service.getAllAchievement();
+    }
+    @GetMapping("/app-user")
+    public Achievement getAchievementByAppUserID (@PathVariable("app-user") UUID appuserId){
+        return service.getAchievementByAppUserID(appuserId);
+    }
 }
